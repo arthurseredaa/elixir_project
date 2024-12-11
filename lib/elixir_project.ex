00f8ca6b50fe0d0a4af04b7fa2e7ce3704b5c28b
arthursereda <arthur.sereda@promova.com>
2024-12-11 23:54:19 +0200

    Initial commit

---
+++ lib/elixir_project.ex
@@ -0,0 +1,22 @@
+defmodule ElixirProject do
+  @moduledoc """
+  Documentation for `ElixirProject`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def sum(x, y) do
+    IO.puts(x + y)
+  end
+end
