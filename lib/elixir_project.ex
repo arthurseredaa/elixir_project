948ce2f8df93ee716742c07df89f592f21014e11
arthursereda <arthur.sereda@promova.com>
2024-12-15 22:48:17 +0200

    Update guessing game with error handling

--- lib/elixir_project.ex
+++ lib/elixir_project.ex
@@ -12,12 +12,17 @@ defmodule ElixirProject do
 
   def main do
     correct = Enum.random(0..10)
-    user_guess = IO.gets("Guess the number between 0 and 10: ") |> String.trim() |> String.to_integer()
+    user_guess = IO.gets("Guess the number between 0 and 10: ") |> String.trim() |> Integer.parse()
 
-    if (user_guess == correct) do
-      IO.puts("You got it!🎉")
-    else
-      IO.puts("Correct answer is #{correct}\nTry next time🫣")
+    case user_guess do
+      {result, _} ->
+        if result === correct do
+          IO.puts("You win!")
+        else
+          IO.puts("You lose! Guessed number is: #{correct}")
+        end
+
+      :error -> IO.puts("Incorrect answer format. Please, use only numbers")
     end
   end
 end
