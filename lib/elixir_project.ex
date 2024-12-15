4cfeabb95dfeca7c0a04dace13664df2d323e256
arthursereda <arthur.sereda@promova.com>
2024-12-15 13:01:59 +0200

    Elixir structs

--- lib/elixir_project.ex
+++ lib/elixir_project.ex
@@ -1,6 +1,17 @@
+defmodule Membership do
+  defstruct [:type, :price]
+end
+
+defmodule User do
+  defstruct [:name, :membership]
+end
+
 defmodule ElixirProject do
   use Application
 
+  @moduledoc """
+    Module for studying
+  """
   def start(_type, _args) do
     main()
 
@@ -8,12 +19,32 @@ defmodule ElixirProject do
   end
 
   def main do
-    name = "Caleb"
-    status = Enum.random([:gold, :silver, :bronse])
+    gold_membership = %Membership{
+      type: :gold,
+      price: 50,
+    }
+    silver_membership = %Membership{
+      type: :silver,
+      price: 25,
+    }
+    bronze_membership = %Membership{
+      type: :bronze,
+      price: 10,
+    }
+    no_membership = %Membership{
+      type: :none,
+      price: 0,
+    }
 
-    case status do
-      :gold -> IO.puts("Welcome to the fancy lounge, #{name}")
-      _ -> IO.puts("Get out bruh")
-    end
+    users = [
+      %User{name: "Caleb", membership: gold_membership},
+      %User{name: "Jacob", membership: silver_membership},
+      %User{name: "Alice", membership: bronze_membership},
+      %User{name: "Arthur", membership: no_membership}
+    ]
+
+    Enum.each(users, fn %User{name: name, membership: membership} ->
+      IO.puts("#{name} has a #{membership.type} membership, that cost #{membership.price}")
+    end)
   end
 end
